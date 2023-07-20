5b762aeb84e5efeef2c012f1bfd45614fb022c6c
Vladislav <20031911@mail.ru>
2023-07-20 17:28:46 +0300

    Декомпозиция проекта

--- scripts/fontains.js
+++ scripts/fontains.js
@@ -1,41 +1,34 @@
-// первоначальное создание карты
-
 import points from "../data/data.js";
 
-var map = L.map('map', {
-}).setView([44.599762035793084, 40.10297859115561], 20);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+export function drawFontains(map) {
 
 // проекция точки на карту
 // let pnt = L.point([4464243.156060995, 5528717.535569337])
 // let ll = L.CRS.EPSG3395.unproject(pnt)
 
-
-let poin = points.map((a) => {
-    let pointFromCoord = L.point(...a.geometry.coordinates)
-    let latLongProj = L.CRS.EPSG3395.unproject(pointFromCoord)
-    a.geometry.coordinates = [latLongProj.lat, latLongProj.lng]
-    return a
-})
-
-poin.forEach((point) => {
-    let circle = L.circle(point.geometry.coordinates, {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.5,
-        radius: 20,
-    }).addTo(map)
-
-    // добавление зума на точку при нажатии
-    circle.addEventListener('click', (e) => {
-        map.fitBounds(e.target.getBounds());
+// проецирование координат точек из формата EPSG3395 в формат EPSG4326
+    let poin = points.map((a) => {
+        let pointFromCoord = L.point(...a.geometry.coordinates)
+        let latLongProj = L.CRS.EPSG3395.unproject(pointFromCoord)
+        a.geometry.coordinates = [latLongProj.lat, latLongProj.lng]
+        return a
     })
 
-    // добавление всплывающего окна
-    circle.bindPopup(`<h1>${point.objectName}</h1>
+    poin.forEach((point) => {
+        let circle = L.circle(point.geometry.coordinates, {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius: 20,
+        }).addTo(map)
+
+        // добавление зума на точку при нажатии
+        circle.addEventListener('click', (e) => {
+            map.fitBounds(e.target.getBounds());
+        })
+
+        // добавление всплывающего окна
+        circle.bindPopup(`<h1>${point.objectName}</h1>
     <h2>Id: ${point.id}</h2>`)
-})
+    })
+}
---
+++ scripts/map.js
@@ -0,0 +1,12 @@
+import {drawFontains} from './fontains.js'
+
+// первоначальное создание карты
+var map = L.map('map', {
+}).setView([44.599762035793084, 40.10297859115561], 15);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+drawFontains(map)
