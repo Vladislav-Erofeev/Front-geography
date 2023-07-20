4d7d1e5e2be6bd486e553db434fc0a77255a9c47
Vladislav <20031911@mail.ru>
2023-07-20 16:50:55 +0300

    добавлен тестовый датасет

---
+++ data/data.js
@@ -0,0 +1,115 @@
+const points = [
+    {
+        "id": 4,
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                4464243.156060995,
+                5528717.535569337
+            ]
+        },
+        "id2": 1,
+        "uuid": "",
+        "code": "31650000",
+        "objectName": "Фонтаны",
+        "caption": "",
+        "cont": 0
+    },
+    {
+        "id": 6,
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                4465012.755162954,
+                5529471.519839273
+            ]
+        },
+        "id2": 3,
+        "uuid": "",
+        "code": "31650000",
+        "objectName": "Фонтаны",
+        "caption": "",
+        "cont": 0
+    },
+    {
+        "id": 7,
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                4465973.406958648,
+                5532603.46914412
+            ]
+        },
+        "id2": 0,
+        "uuid": "",
+        "code": "31650000",
+        "objectName": "Фонтаны",
+        "caption": "Девушка с кушином",
+        "cont": 0
+    },
+    {
+        "id": 8,
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                4464599.563320995,
+                5529794.0850912
+            ]
+        },
+        "id2": 2,
+        "uuid": "",
+        "code": "31650000",
+        "objectName": "Фонтаны",
+        "caption": "",
+        "cont": 0
+    },
+    {
+        "id": 9,
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                4464267.427748488,
+                5529247.539805277
+            ]
+        },
+        "id2": 0,
+        "uuid": "",
+        "code": "31650000",
+        "objectName": "Фонтаны",
+        "caption": "",
+        "cont": 0
+    },
+    {
+        "id": 10,
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                4334365.576386872,
+                5588342.50726478
+            ]
+        },
+        "id2": 0,
+        "uuid": "",
+        "code": "31650000",
+        "objectName": "Фонтаны",
+        "caption": "",
+        "cont": 0
+    },
+    {
+        "id": 11,
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                4335203.8305927375,
+                5594296.218989824
+            ]
+        },
+        "id2": 0,
+        "uuid": "",
+        "code": "31650000",
+        "objectName": "Фонтаны",
+        "caption": "",
+        "cont": 0
+    }
+]
+export default points
